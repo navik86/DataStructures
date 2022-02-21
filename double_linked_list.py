e78df2ee13040ce52eb7f7470ffa0d64ef68ea7c
Ivan Kurban <ivankurban1986@gmail.com>
2022-02-21 10:55:23 +0300

    Change double_linked_list.py

--- double_linked_list.py
+++ double_linked_list.py
@@ -9,15 +9,35 @@ class Node:
 
 class DoubleLinkedList:
 
-    # инициализируем первый элемент связанного списка
-    # т.е. список создается с head
+    # инициализируем пустой двухсвязанный список
     def __init__(self):
         self.head = None
 
+    # метод добавляет элемент в начало списка
+    def push(self, new_data):
+        # создаем новый элемент списка, указываем знач.
+        new_node = Node(new_data)
+
+        # ссылку даем на существующий объект head
+        new_node.next = self.head
+
+        # если head не None:
+        if self.head:
+            self.head.prev = new_node
+
+        # головой становится недавносозданный объект
+        self.head = new_node
 
 
 
 
+
+    # вывод всех элементов связанного списка (перебор)
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=' ')
+            temp = temp.next
 
 
 
@@ -25,3 +45,9 @@ class DoubleLinkedList:
 if __name__=='__main__':
 
     my_list = DoubleLinkedList()
+
+    print('Создадим двухсвязанный список:')
+    for i in range(6):
+        my_list.push(i)
+
+    my_list.printList()
