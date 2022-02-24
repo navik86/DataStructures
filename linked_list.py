be0e781dd8376df06533fa36b664fcced14fe69b
Ivan Kurban <ivankurban1986@gmail.com>
2022-02-24 23:42:15 +0300

    Add push_back and new print_list

--- linked_list.py
+++ linked_list.py
@@ -44,23 +44,30 @@ def create_array():
     array = UserList()
     array.head = None
     array.tail = None
+    array.counter = 0
     return array
 
 
 def push_front(ll, new_pair):
     if ll.head is None:
         ll.head = ll.tail = new_pair
-        return
-    if pair_second(ll.head) is None:
+    elif pair_second(ll.head) is None:
         ll.head = new_pair
         set_second(ll.head, ll.tail)
     else:
         set_second(new_pair, ll.head)
         ll.head = new_pair
+    ll.counter += 1
     return ll
 
 
-def push_back(ll, pair):
+def push_back(ll, new_pair):
+    if ll.head is None:
+        ll.head = ll.tail = new_pair
+    else:
+        set_second(ll.tail, new_pair)
+        ll.tail = new_pair
+    ll.counter += 1
     return ll
 
 
@@ -79,7 +86,7 @@ def swap_pair(ll, x, y):
 def iter_ll(ll):
     current = ll.head
     while current:
-        item_val = pair_first(current)
+        item_val = current
         current = pair_second(current)
         yield item_val
 
@@ -87,6 +94,17 @@ def iter_ll(ll):
 def print_list(ll):
     for node in iter_ll(ll):
         print(node, end=' ')
+
+
+# def print_list(ll):
+#     current = ll.head
+#     last = None
+#     while last != ll.head:
+#         while pair_second(current) != last:
+#             current = pair_second(current)
+#         last = current
+#         print(last)
+#         current = ll.head
 
 
 def my_sort(ll):
@@ -98,15 +116,18 @@ if __name__ == '__main__':
     cf = create_first('f1')
     cf2 = create_first('f2')
     cf3 = create_first('f3')
+    cf4 = create_first('f4')
     cs = create_second(None)
     cp = create_pair(cf, cs)
     cp2 = create_pair(cf2, cs)
     cp3 = create_pair(cf3, cs)
+    cp4 = create_pair(cf4, cs)
 
     my_ll = create_ll(create_array())
     push_front(my_ll, cp)
     push_front(my_ll, cp2)
     push_front(my_ll, cp3)
+    push_back(my_ll, cp4)
 
     print_list(my_ll)
 
