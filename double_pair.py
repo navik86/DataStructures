7340027e295743a0a5d0531bd921a568e44af634
Ivan Kurban <ivankurban1986@gmail.com>
2022-02-28 13:27:15 +0300

    Editing double_pair create_dp

--- double_pair.py
+++ double_pair.py
@@ -10,16 +10,16 @@ class DoublePair:
 def create_dp(data):
     a = create_pair(data)
     b = create_pair()
+    pair_second(b)
     return DoublePair(a, b)
 
 
 def get_data(double_pair):
-    print(double_pair.data)
-    print(double_pair.ref)
+    return pair_first(double_pair.data)
 
 
-def get_ref_pair(pair):
-    return pair.b
+def get_ref_pair(double_pair):
+    return double_pair.ref
 
 
 def get_prev(pair):
@@ -50,6 +50,6 @@ if __name__ == '__main__':
 
     dp1 = create_dp(1)
 
-    get_data(dp1)
+    print(get_data(dp1))
 
 
