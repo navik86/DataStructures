93dbb0badb241469919af676000a0a5e9320ebc8
Ivan Kurban <ivankurban1986@gmail.com>
2022-03-01 13:46:59 +0300

    Add pop() in stack

--- stack.py
+++ stack.py
@@ -18,9 +18,38 @@ class Stack:
             self.top = new_pair
         self.size += 1
 
+    # [..], [..], [..] -> [..]
+    def pop(self):
+        if self.size == 0:
+            return
+        elif self.size == 1:
+            self.top = None
+        else:
+            self.top = get_prev(self.top)
+            set_next(self.top, None)
+        self.size -= 1
+
+    def print_dll(self):
+        if self.size == 0:
+            return
+        current = self.top
+        a = []
+        while current:
+            # print(get_data(current), end=' ')
+            a.append(get_data(current))
+            current = get_prev(current)
+        [print(i, end=' ') for i in reversed(a)]
+
 
 if __name__ == '__main__':
 
     st = Stack()
     st.push(1)
     st.push(2)
+    st.push(3)
+
+    st.print_dll()
+    print()
+
+    st.pop()
+    st.print_dll()
