a97184ec5e50cf0e85999217aade49ac4604f116
Ivan Kurban <ivankurban1986@gmail.com>
2022-03-02 15:12:04 +0300

    Changed double_pair

--- double_pair.py
+++ double_pair.py
@@ -1,13 +1,5 @@
 from obj_pair import *
 
-# class DoublePair:
-#     def __init__(self, data=None, ref=None):
-#         self.data = data
-#         self.ref = ref
-#
-#     def __str__(self):
-#         return f"{self.data}"
-
 
 def create_dp(data):
     links = create_pair()
@@ -29,34 +21,21 @@ def get_next(double_pair):
     return pair_second(pair_second(double_pair))
 
 
-# [data, ...] -> [... , ...]
+# [data, [... , ...]]
 def set_data(double_pair, data):
-    return set_first(double_pair.data, data)
+    return set_first(double_pair, data)
 
 
-# [... , ref] - [... , ...]
-def set_ref_pair(double_pair, ref):
-    return set_second(double_pair.data, ref)
-
-
-# [... , ...] -> [prev , ...]
+# [..., [prev, ...]]
 def set_prev(double_pair, prev):
-    set_first(double_pair.ref, prev)
+    return set_first(pair_second(double_pair), prev)
 
 
-# [... , ...] -> [... , next]
+# [..., [..., next]]
 def set_next(double_pair, next):
-    set_second(double_pair.ref, next)
+    return set_second(pair_second(double_pair), next)
 
 
-if __name__ == '__main__':
-
-    dp1 = create_dp(1)
-    dp2 = create_dp(2)
-    dp3 = create_dp(3)
-
-    set_prev(dp2, dp1)
-    set_next(dp2, dp3)
 
 
 
