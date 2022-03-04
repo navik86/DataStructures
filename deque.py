d6c38b7a5b62c8dfbbdb6b53be9a9bcb9464605c
Ivan Kurban <ivankurban1986@gmail.com>
2022-03-04 10:45:10 +0300

    Add deque

---
+++ deque.py
@@ -0,0 +1,38 @@
+from double_linked_list import *
+
+
+# insertFront, insertRear, deleteFront, deleteRear
+# getFront, getRear, isEmpty, size
+class MyDeque:
+    def __init__(self):
+        self.dll = DoubleLinkedList()
+
+    # [..] -> [front], [..], [..], [rear]
+    def insertFront(self, item):
+        self.dll.push(item)
+
+    # [front], [..], [..], [rear] <- [..]
+    def insertRear(self, item):
+        self.dll.append(item)
+
+    def deleteFront(self):
+        self.dll.remove()
+
+    def deleteRear(self):
+        self.dll.pop()
+
+    def getFront(self):
+        return self.dll.head
+
+    def getRear(self):
+        return self.dll.last
+
+    def isEmpty(self):
+        if self.size == -1:
+            return True
+        else:
+            return False
+
+    def size(self):
+        a = self.dll.counter + 1
+        return a
